62f0a4195ef9756501c8c5a541002edbc2bead24
Fangfang Xia <fangfang.xia@gmail.com>
2014-10-25 10:22:48 -0500

    add env for beagle compute

---
+++ env/compute-env-for-beagle.sh
@@ -0,0 +1,15 @@
+export VIRTUAL_ENV=/lustre/beagle/fangfang/py-arast-env
+export PATH=$VIRTUAL_ENV/bin:$PATH
+export LD_LIBRARY_PATH=/lustre/beagle/fangfang/py-arast-env/lib/python2.7:$LD_LIBRARY_PATH
+export PYTHONPATH=/lustre/beagle/fangfang/py-arast-env/lib/python2.7/site-package
+
+export ARAST_URL=140.221.67.210
+export ARAST_QUEUE=beagle
+
+export ARAST_LIB_DIR=/lustre/beagle/fangfang/assembly/lib
+export ARAST_VAR_DIR=/lustre/beagle/fangfang/deployment/var
+export ARAST_BIN_DIR=/lustre/beagle/fangfang/arast/third_party
+export ARAST_DATA_DIR=/lustre/beagle/fangfang/arast/data
+
+export ARAST_WORKER_THREADS=2
+
